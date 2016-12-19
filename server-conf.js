e5b1b02f3753dd561729982d6d452048ba6f553d
wuhy <sparklewhy@gmail.com>
2016-12-19 11:09:01 +0800

    remove livereload

--- server-conf.js
+++ server-conf.js
@@ -37,8 +37,7 @@ exports.getLocations = function () {
             location: /\/$/,
             handler: [
                 home('index.html'),
-                requireInjector,
-                livereload()
+                requireInjector
             ]
         },
         {
